3af0ff3850a59574efa3025b5674b101ca4019c5
xion <974356215@qq.com>
2024-12-23 17:24:26 +0800

    SMPLX npz

---
+++ my_src/my_human_body_prior/body_model/body_model.py
@@ -0,0 +1,18 @@
+# !/usr/bin/python3
+# _*_ coding: utf-8 _*_
+# @Time     : 12/23/24 4:31 PM
+# @Author   : YeYiqi
+# @Email    : yeyiqi@stu.pku.edu.cn
+# @File     : body_model.py.py
+# @desc     :
+
+import numpy as np
+import torch
+import torch.nn as nn
+from lbs import lbs
+import sys
+
+class
+
+
+
